510cd1b8463a336f091344fa076e06984a864b2a
Noppanut <noppanut.jang2547@gmail.com>
2023-08-06 23:11:11 +0700

    completely project

--- main/src/script.js
+++ main/src/script.js
@@ -1,5 +1,27 @@
-let slideindex = 1;
+let slideshows = document.getElementsByClassName('slideshows');
 
-function slideshow(n) {
-    
+let slideIndex = 1;
+showSlide(slideIndex)
+
+function plusSlides(n) {
+    showSlide(slideIndex += n)
+}
+
+function currentSlide(n) {
+    showSlide(slideIndex = n)
+}
+function showSlide(n) {
+    let i;
+    let slides = document.getElementsByClassName('slideshows');
+    let dots = document.getElementsByClassName('dot');
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
 }
